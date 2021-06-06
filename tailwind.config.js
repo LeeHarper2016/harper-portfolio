f8347b545c928f4a9a3d9e9b87f8d0cf5d5402ab
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-06 12:00:45 -0500

    Update tailwind.config.js
    Updated: tailwind.config.js
    -> Added a 'header-bg' image directive, which will allow the developer to use it in tailwindcss.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+              'header-bg': "url('/images/header-bg.jpg')"
+            })
+    },
   },
   variants: {
     extend: {},
