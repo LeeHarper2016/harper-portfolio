3dff978a70515595e5a6c344f64e351faef63b98
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-07 11:03:45 -0500

    Update app.js
    Updated: app.js
    -> Added the Modal Vue component as a component for the application.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,11 @@ require('./bootstrap');
 
 import Vue from 'vue';
 
+import Modal from './components/Modal';
+
 window.Vue = new Vue({
 	el: '#app',
-	components: {}
+	components: {
+		Modal
+	}
 });
