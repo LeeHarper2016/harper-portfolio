c66d79c82c826e6947e6aa32238d29d41534798c
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-09 15:08:31 -0500

    Update AuthController.php
    Updated: AuthController.php
    -> Implemented a function that takes information supplied by the user, and then attempts to authenticate them to the server.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    /*********************************************************************************************************************************************************************
+     * 
+     * Function Name: AuthController.logInUser(Request $request).
+     * Purpose: Determines if the provided credentials are accurate, and if they are logs the user in. If not, return an error.
+     * Precondition: N/A.
+     * Postcondition: N/A.
+     * 
+     * @param Request $request the entire http request.
+     * 
+     * ******************************************************************************************************************************************************************/
+    public function logInUser(Request $request) {
+        $credentials = $request->only('name', 'password');
+
+        if(Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return back();
+        } else {
+            return back()->withErrors(['The credentials provided did not match our records.']);
+        }
+    }
 }
