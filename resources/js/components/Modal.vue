ddee9afc37bc4921b037e3585240ee0b2415dde3
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-09 09:20:21 -0500

    Update Modal.vue
    Updated: Modal.vue
    -> Implemented functionality for the modal fading in and out of visibility.

--- resources/js/components/Modal.vue
+++ resources/js/components/Modal.vue
@@ -4,20 +4,28 @@
 				class="p-3 bg-red-400 rounded-lg border-purple-800 text-center">
 			Edit
 		</button>
-		<div v-if="visible" 
-			 class="w-screen h-screen fixed bg-gray-600 bg-opacity-75 top-0 left-0 z-10 p-24"
-			 @click="toggleVisibility">
-			<div class="rounded-lg bg-white w-full h-full p-10"
-				 @click.stop>
-				<h2 class="text-center font-bold text-2xl">
-					<slot name="header"></slot>
-				</h2>
+		<transition name="fade">
+			<div v-if="visible" 
+				 class="w-screen h-screen fixed bg-gray-600 bg-opacity-75 top-0 left-0 z-10 p-24"
+				 @click="toggleVisibility">
+				<div class="rounded-lg bg-white w-full h-full p-10"
+					 @click.stop>
+					<h2 class="text-center font-bold text-2xl">
+						<slot name="header"></slot>
+					</h2>
+				</div>
 			</div>
-		</div>
+		</transition>
 	</div>
 </template>
 
 <style scoped>
+.fade-enter-active, .fade-leave-active {
+	transition: opacity 0.75s ease;
+}
+.fade-enter, .fade-leave-to {
+	opacity: 0;
+}
 </style>
 
 <script type="text/javascript">
