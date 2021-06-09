ecae53348fa67e9f642b36185bfad6ebfef1ce3a
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-09 13:31:08 -0500

    Create login-form.blade.php
    Created: login-form.blade.php
    -> Refactored the login form into its own component.

---
+++ resources/views/components/login-form.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="POST" class="flex flex-col items-center space-y-6 font-normal">
+        <label for="username">
+            Username:
+            <input type="text" name="username" id="username" class="border rounded-md block">
+        </label>
+        <label for="password">
+            Password:
+            <input type="password" name="password" id="password" class="border rounded-md block">
+        </label>
+        <button type="submit" class="border border-purple-800 rounded-lg p-3 bg-purple-300">
+            Log In
+        </button>
+    </form>
+</div>
