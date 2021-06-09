d8d690668661e22d952a91f5f29bf549e54af49a
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-09 13:31:32 -0500

    Update welcome.blade.php
    Updated: welcome.blade.php
    -> Replaced the login form with the newly refactored login-form component.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,19 +32,7 @@
                             Log In
                         </template>
                         <template #content>
-                            <form action="" method="POST" class="flex flex-col items-center space-y-6 font-normal">
-                                <label for="username">
-                                    Username:
-                                    <input type="text" name="username" id="username" class="border rounded-md block">
-                                </label>
-                                <label for="password">
-                                    Password:
-                                    <input type="password" name="password" id="password" class="border rounded-md block">
-                                </label>
-                                <button type="submit" class="border border-purple-800 rounded-lg p-3 bg-purple-300">
-                                    Log In
-                                </button>
-                            </form>
+                            <x-login-form />
                         </template>
                     </modal>
                     <a href="#education" class="hover:text-gray-800 text-xl duration-300">Education</a>
