53ece61a09f7a260bc6780f41a9d40cd3d3b7ad7
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-10 13:14:22 -0500

    Update welcome.blade.php
    Updated: welcome.blade.php
    -> Implemented an action bar for actions that a user can perform on the application.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,38 +73,40 @@
                 </div>
             </main>
             @auth
-            <modal button_classes="ml-auto mr-auto rounded-full border-2 border-purple-800 font-bold p-2 w-10 text-center mb-10 cursor-pointer">
-                <template #button_text>
-                    +
-                </template> 
-                <template #header>
-                    Add A Section
-                </template>
-                <template #content><div>
-                    <form action="/components" method="POST" class="flex flex-col items-center space-y-6 font-normal" enctype="multipart/form-data">
-                        @csrf
-                        <label for="anchor">
-                            Anchor Tag:
-                            <input type="text" name="anchor" id="anchor" class="border rounded-md block">
-                        </label>
-                        <label for="title">
-                            Section Title:
-                            <input type="text" name="title" id="title" class="border rounded-md block">
-                        </label>
-                        <label for="body">
-                            Section Body:
-                            <input type="text" name="body" id="body" class="border rounded-md block">
-                        </label>
-                        <label for="image">
-                            Section Image:
-                            <input type="file" name="image" id="image" class="border rounded-md block">
-                        </label>
-                        <button type="submit" class="border border-purple-800 rounded-lg p-3 bg-purple-300">
-                            Submit
-                        </button>
-                    </form>
-                </template>
-            </modal>
+            <div class="mr-auto ml-auto w-1/2 border border-purple-800 rounded-lg flex justify-around p-5 mb-10">
+                <modal button_classes="rounded-full border-2 border-purple-800 font-bold p-2 w-10 text-center cursor-pointer">
+                    <template #button_text>
+                        +
+                    </template> 
+                    <template #header>
+                        Add A Section
+                    </template>
+                    <template #content><div>
+                        <form action="/components" method="POST" class="flex flex-col items-center space-y-6 font-normal" enctype="multipart/form-data">
+                            @csrf
+                            <label for="anchor">
+                                Anchor Tag:
+                                <input type="text" name="anchor" id="anchor" class="border rounded-md block">
+                            </label>
+                            <label for="title">
+                                Section Title:
+                                <input type="text" name="title" id="title" class="border rounded-md block">
+                            </label>
+                            <label for="body">
+                                Section Body:
+                                <input type="text" name="body" id="body" class="border rounded-md block">
+                            </label>
+                            <label for="image">
+                                Section Image:
+                                <input type="file" name="image" id="image" class="border rounded-md block">
+                            </label>
+                            <button type="submit" class="border border-purple-800 rounded-lg p-3 bg-purple-300">
+                                Submit
+                            </button>
+                        </form>
+                    </template>
+                </modal>
+            </div>
             @endauth
         </div>
     </body>
