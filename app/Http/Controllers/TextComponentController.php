af89d0acf1f68e4142a0e221e9cfa9cc4bbb28f9
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-10 14:26:45 -0500

    Update TextComponentController.php
    Updated: TextComponentController.php
    -> Implemented a store() method, which will be used to store a new TextComponent model in the database.

--- app/Http/Controllers/TextComponentController.php
+++ app/Http/Controllers/TextComponentController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\TextComponentRequest;
+use App\Models\TextComponent;
 
 class TextComponentController extends Controller
 {
-    //
+    /*********************************************************************************************************************************************************************
+     * 
+     * Function Name: TextComponentController.store().
+     * Purpose: Using the validated information from $request, a new component will be created and stored to the database.
+     * Precondition: The $request information is valid.
+     * Postcondition: A new text component is stored to the database.
+     * 
+     * @param TextComponentRequest $request The validated information that has been supplied by the user.
+     * @returns Redirection back to the homepage.
+     * 
+     ********************************************************************************************************************************************************************/
+    public function store(TextComponentRequest $request) {
+        $component = new TextComponent;
+        $componentImage = $request->image;
+
+        TextComponent::create([
+            "anchor" => $request->anchor,
+            "title" => $request->title,
+            "body" => $request->body,
+            "img_src" => $componentImage->hashName()
+        ]);
+
+        $componentImage->store('/images/uploaded');
+
+        return back();
+    }
 }
