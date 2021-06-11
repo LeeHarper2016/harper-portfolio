748847cfef6366db202f96ba0cd8deba1a3f5e6b
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-11 10:39:33 -0500

    Update ErrorMessage.vue
    Updated: ErrorMessage.vue
    -> Implemented functionality for closing out of error messages.
    -> Implemented a fade effect for when the error is closed out of.

--- resources/js/components/ErrorMessage.vue
+++ resources/js/components/ErrorMessage.vue
@@ -1,7 +1,12 @@
 <template>
-    <div class="w-3/4 bg-red-400 rounded-lg p-3 ml-auto mr-auto">
-        {{ error }}
-    </div>
+	<transition name="fade">
+	    <div class="flex justify-between w-3/4 bg-red-400 rounded-lg p-3 ml-auto mr-auto"
+	         v-show="isVisible">
+	        <span>{{ error }}</span>
+	        <span @click="toggleVisibility"
+	        	  class="cursor-pointer font-bold">X</span>
+	    </div>
+	</transition>
 </template>
 
 <style scoped>
@@ -19,6 +24,12 @@
 		props: ['error'],
 		data() {
 			return {
+				isVisible: true
+			}
+		},
+		methods: {
+			toggleVisibility() {
+				this.isVisible = !this.isVisible;
 			}
 		}
 	};
