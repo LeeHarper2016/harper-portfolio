f86c529be9f8735f8fb1c9caaf5c1741b6517467
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-11 09:02:56 -0500

    Update HomeController.php
    Updated: HomeController.php
    -> Implemented a function to get all text components from the database, then returns a view with the gathered data.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\TextComponent;
 
 class HomeController extends Controller
 {
-    //
+    /***********************************************************************************************************************************************************
+     * 
+     * Function Name: HomeController.view().
+     * Purpose: Returns the welcome page view along with all sections stored in the database.
+     * Precondition: N/A.
+     * Postcondition: N/A.
+     * 
+     * @returns response The welcome page view with data from the database.
+     * 
+     **********************************************************************************************************************************************************/
+    public function view() {
+        $data = [
+            'sections' => TextComponent::all()
+        ];
+
+        return response()->view('welcome', $data);
+    }
 }
