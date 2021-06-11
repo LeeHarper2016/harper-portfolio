e37370ff4ddb5d01911ba27cab13c3fd345b7aed
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-11 09:36:51 -0500

    Update welcome.blade.php
    Updated: welcome.blade.php
    -> Implemented form for editing the 'About Me' section.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,6 +51,38 @@
                         <template #header>
                             Test
                         </template>
+                        <template #content>
+                            <form action="/component/text" method="POST" class="flex flex-col items-center space-y-6 font-normal" enctype="multipart/form-data">
+                                @csrf
+                                <label for="anchor">
+                                    Anchor Tag:
+                                    <input type="text" name="anchor" id="anchor" class="border rounded-md block">
+                                </label>
+                                <label for="title">
+                                    Section Title:
+                                    <input type="text" name="title" id="title" class="border rounded-md block">
+                                </label>
+                                <label for="body">
+                                    Section Body:
+                                    <textarea cols="50" rows="5" id="body" name="body" class="border rounded-md block">
+                                        
+                                    </textarea>
+                                </label>
+                                <label for="image">
+                                    Section Image:
+                                    <input type="file" name="image" id="image" class="border rounded-md block">
+                                </label>
+                                <label for="img_alt">
+                                    Image Description:
+                                    <textarea cols="50" rows="5" id="img_alt" name="img_alt" class="border rounded-md block">
+                                        
+                                    </textarea>
+                                </label>
+                                <button type="submit" class="border border-purple-800 rounded-lg p-3 bg-purple-300">
+                                    Submit
+                                </button>
+                            </form>
+                        </template>
                     </modal>
                     <hr class="border-purple-800 w-1/4">
                     <div class="flex justify-between">
