a97f5b91c5339fa073af7d7dab7f858204cfed5d
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-11 11:43:15 -0500

    Create ExperienceSlideshow.vue
    Created: ExperienceSlideshow.vue
    -> This vue component will, when complete, be responsible for displaying a slideshow of the work experience found in the database.

---
+++ resources/js/components/ExperienceSlideshow.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="flex w-full bg-purple-200 border-t-2 border-b-2 border-purple-800">
+	    <a name="experience"></a>
+	    <div class="w-1/2 h-96">
+	    </div>
+	    <img src="{{ asset('/images/default.jpg') }}" alt="test" class="w-1/2 h-96">
+	</div>
+</template>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+	transition: opacity 0.65s ease;
+}
+.fade-enter, .fade-leave-to {
+	opacity: 0;
+}
+</style>
+
+<script type="text/javascript">
+	export default {
+		name: 'ExperienceSlideshow',
+		data() {
+			return {
+				positions: {
+					company_name: '',
+					position: '',
+					body: '',
+					img_src: '',
+					img_alt: ''
+				}
+			}
+		}
+	};
+</script>
