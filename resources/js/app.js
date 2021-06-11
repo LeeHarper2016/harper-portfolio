b5d1411751d4055b61f5d7a496dd8c1d7dd26f5b
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-11 11:43:40 -0500

    Update app.js
    Update: app.js
    -> Implemented the ExperienceSlideshow component for testing.

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,14 @@ require('./bootstrap');
 import Vue from 'vue';
 
 import ErrorMessageList from './components/ErrorMessageList';
+import ExperienceSlideshow from './components/ExperienceSlideshow';
 import Modal from './components/Modal';
 
 window.Vue = new Vue({
 	el: '#app',
 	components: {
 		Modal,
-		ErrorMessageList
+		ErrorMessageList,
+		ExperienceSlideshow
 	}
 });
