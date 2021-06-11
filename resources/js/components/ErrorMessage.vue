75b8bd9aa5ce6648551c795d611a27c82b8588df
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-11 10:10:51 -0500

    Create ErrorMessage.vue
    Created: ErrorMessage.vue
    -> This will be responsible for showing an individual error to the user.

---
+++ resources/js/components/ErrorMessage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="w-3/4 bg-red-400 rounded-lg p-3 ml-auto mr-auto">
+        {{ error.message }}
+    </div>
+</template>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+	transition: opacity 0.65s ease;
+}
+.fade-enter, .fade-leave-to {
+	opacity: 0;
+}
+</style>
+
+<script type="text/javascript">
+	export default {
+		name: 'ErrorMessage',
+		props: ['error'],
+		data() {
+			return {
+			}
+		}
+	};
+</script>
