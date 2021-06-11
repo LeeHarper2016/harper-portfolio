adee7ca98ff9b00ab71c2444415a840eaf7fd7ea
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-11 13:21:25 -0500

    Update ExperienceSlideshow.vue
    Updated: ExperienceSlideshow.vue
    -> Adjusted the styling of the base component.
    -> Added placeholder text for the time being.

--- resources/js/components/ExperienceSlideshow.vue
+++ resources/js/components/ExperienceSlideshow.vue
@@ -1,9 +1,19 @@
 <template>
 	<div class="flex w-full bg-purple-200 border-t-2 border-b-2 border-purple-800">
 	    <a name="experience"></a>
-	    <div class="w-1/2 h-96">
+	    <div class="w-1/2 h-96 p-5 space-y-3">
+	    	<h2 class="font-bold text-2xl">H1 Electrical Services, LLC.</h2>
+	    	<span class="italic">Full Stack Laravel Developer</span>
+	    	<p>
+	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	    	</p>
 	    </div>
-	    <img src="" alt="test" class="w-1/2 h-96">
+	    <img src="/images/default.jpg" alt="test" class="w-1/2 h-96">
 	</div>
 </template>
 
