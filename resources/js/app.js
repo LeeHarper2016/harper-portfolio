40c6ff789883c7fed9de21c93eaa590b0e226a3d
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-11 10:40:39 -0500

    Update app.js
    Updated: app.js
    -> Implemented the ErrorMessageList vue component.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,13 @@ require('./bootstrap');
 
 import Vue from 'vue';
 
+import ErrorMessageList from './components/ErrorMessageList';
 import Modal from './components/Modal';
 
 window.Vue = new Vue({
 	el: '#app',
 	components: {
-		Modal
+		Modal,
+		ErrorMessageList
 	}
 });
