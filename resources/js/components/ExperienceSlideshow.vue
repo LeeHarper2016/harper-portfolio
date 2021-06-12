5c08db5ac52d4d8671e914285dbeb50ae3ff15bb
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-12 08:37:51 -0500

    Update ExperienceSlideshow.vue
    Updated: ExperienceSlideshow.vue
    ->Added functionality for if the positions variable is 0 or 1.
    -> Implemented usage of the positions array, which will be used to populate the slideshwo.
    -> Implemented a "more info" modal that the user can click on to learn more about the experience of the current position.
    -> Implemented an async mounted() method, which will populate the positions array with information gathered from the '/api/positions' route.

--- resources/js/components/ExperienceSlideshow.vue
+++ resources/js/components/ExperienceSlideshow.vue
@@ -1,28 +1,60 @@
 <template>
-	<div class="flex w-full bg-purple-200 border-t-2 border-b-2 border-purple-800">
+	<div v-if="positions.length === 0"
+		 class="flex w-full bg-purple-200 border-t-2 border-b-2 border-purple-800 p-5">
+		 <div class="flex w-full justify-around">
+		    <Modal class="inline" 
+		    	   button_classes="bg-purple-400 rounded-lg p-3 border border-purple-800">
+		        <template #button_text>
+		        	More Info
+		        </template>
+		        <template #header>
+		        	{{ positions[0].company_name }}
+		        </template>
+		        <template #content>
+		        	{{ positions[0].body }}
+		        </template>
+		    </Modal>
+	    	<ExperienceSlideshowForm></ExperienceSlideshowForm>
+	    	<button class="bg-red-400 rounded-lg p-3 border border-purple-800">
+	    		Delete Position
+	    	</button>
+    	</div>
+	</div>
+	<div v-else-if="positions.length === 1"
+		 class="flex w-full bg-purple-200 border-t-2 border-b-2 border-purple-800">
 	    <a name="experience"></a>
 	    <div class="w-1/2 h-96 p-5 space-y-3">
-	    	<h2 class="font-bold text-2xl">H1 Electrical Services, LLC.</h2>
-	    	<span class="italic">Full Stack Laravel Developer</span>
-	    	<p>
-	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	    	<h2 class="font-bold text-2xl"
+	    		v-text="positions[0].company_name"></h2>
+	    	<span class="italic"
+	    		  v-text="positions[0].position"></span>
+	    	<p v-text="positions[0].description">
 	    	</p>
 	    	<div class="flex w-full justify-around">
-		    	<button class="bg-purple-400 rounded-lg p-3 border border-purple-800">
-		    		More Info
-		    	</button>
+    		    <Modal class="inline" 
+			    	   button_classes="bg-purple-400 rounded-lg p-3 border border-purple-800">
+			        <template #button_text>
+			        	More Info
+			        </template>
+			        <template #header>
+			        	{{ positions[0].company_name }}
+			        </template>
+			        <template #content>
+					    <img :src="positions[0].img_src"
+					    	 :alt="positions[0].img_alt" 
+					    	 class="w-1/2 h-56 ml-auto mr-auto mb-5 rounded-lg border border-purple-800">
+			        	{{ positions[0].body }}
+			        </template>
+			    </Modal>
 		    	<ExperienceSlideshowForm></ExperienceSlideshowForm>
 		    	<button class="bg-red-400 rounded-lg p-3 border border-purple-800">
 		    		Delete Position
 		    	</button>
 	    	</div>
 	    </div>
-	    <img src="/images/default.jpg" alt="test" class="w-1/2 h-96">
+	    <img :src="positions[0].img_src"
+	    	 :alt="positions[0].img_alt" 
+	    	 class="w-1/2 h-96">
 	</div>
 </template>
 
@@ -41,15 +73,20 @@
 		},
 		data() {
 			return {
-				positions: {
+				positions: [{
 					company_name: '',
 					position: '',
 					body: '',
 					description: '',
 					img_src: '',
 					img_alt: ''
-				}
+				}]
 			}
+		},
+		async mounted() {
+			let res = await axios.get('/api/positions');
+
+			this.positions = res.data;
 		}
 	};
 </script>
