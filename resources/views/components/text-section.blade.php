b452d427b165a556216527630dca4a356cdbee3c
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-22 21:27:05 -0500

    Update text-section.blade.php
    Updated: text-section.blade.php
    -> Refactored the html for the text sections into the blade component.

--- resources/views/components/text-section.blade.php
+++ resources/views/components/text-section.blade.php
@@ -1,3 +1,23 @@
-<div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
+<div class="p-8">
+    <a name="{{ $anchor ?? 'default' }}"></a>
+    <h2 class="inline-block font-bold text-3xl mb-3 mr-5">{{ $section->title ?? 'Default Title' }}</h2>
+    <modal class="inline" button_classes="p-1.5 bg-red-400 rounded-lg border-purple-800 text-center">
+        <template #button_text>
+            Edit Section
+        </template>
+        <template #header>
+            Edit Section
+        </template>
+        <template #content>
+            <x-edit-section-form /> 
+        </template>
+    </modal>
+    <hr class="border-purple-800 w-1/4">
+    <div class="flex justify-between">
+        <p class="w-2/3">
+            {{ $section->body ?? 'This is the default text for a section.' }}
+        </p>
+        <img src="{{ isset($section->img_src) ? asset($section->img_src) : asset('/images/default.jpg') }}"
+         alt="{{ $section->img_alt ?? '' }}" class="w-56 h-72 block ml-auto mr-auto rounded-lg border-purple-800 border-4">
+    </div>
 </div>
