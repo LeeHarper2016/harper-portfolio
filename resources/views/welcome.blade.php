bf1cecadb31783d27124decb3d1cea01c549bfee
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-22 19:58:26 -0500

    Update welcome.blade.php
    Updated: welcome.blade.php
    -> Changed the formatting for the education section to be in line with the "About Me" section.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,27 +71,25 @@
                 <experience-slideshow></experience-slideshow>
                 <div class="p-8">
                     <a name="education"></a>
+                    <h2 class="inline-block font-bold text-3xl mb-3 mr-5">{{ $sections[1]->title ?? 'Education' }}</h2>
+                    <modal class="inline" button_classes="p-1.5 bg-red-400 rounded-lg border-purple-800 text-center">
+                        <template #button_text>
+                            Edit Section
+                        </template>
+                        <template #header>
+                            Edit Section
+                        </template>
+                        <template #content>
+                            <x-edit-section-form /> 
+                        </template>
+                    </modal>
+                    <hr class="border-purple-800 w-1/4">
                     <div class="flex justify-between">
+                        <p class="w-2/3">
+                            {{ $sections[1]->body ?? 'This is the default text for a section.' }}
+                        </p>
                         <img src="{{ isset($sections[1]->img_src) ? asset($sections[1]->img_src) : asset('/images/default.jpg') }}"
                          alt="{{ $sections[1]->img_alt ?? '' }}" class="w-56 h-72 block ml-auto mr-auto rounded-lg border-purple-800 border-4">
-                         <div class="w-2/3">
-                            <h2 class="inline-block font-bold text-3xl mb-3 mr-5">{{ $sections[1]->title ?? 'Education' }}</h2>
-                            <modal class="inline" button_classes="p-1.5 bg-red-400 rounded-lg border-purple-800 text-center">
-                                <template #button_text>
-                                    Edit Section
-                                </template>
-                                <template #header>
-                                    Edit Section
-                                </template>
-                                <template #content>
-                                    <x-edit-section-form />
-                                </template>
-                            </modal>
-                            <hr class="border-purple-800 w-1/4">
-                            <p>
-                                {{ $sections[1]->body ?? 'This is the default text for a section.' }}
-                            </p>
-                         </div>
                     </div>
                 </div>
             </main>
