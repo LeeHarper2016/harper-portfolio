095080cc9b73bb6621e7ae8a16110adf6adede34
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-25 18:31:29 -0500

    Update app.js
    Updated: app.js
    -> Implemented the SkillsetShowcase vue component.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,12 +5,14 @@ import Vue from 'vue';
 import ErrorMessageList from './components/ErrorMessageList';
 import ExperienceSlideshow from './components/ExperienceSlideshow';
 import Modal from './components/Modal';
+import SkillsetShowcase from './components/SkillsetShowcase';
 
 window.Vue = new Vue({
 	el: '#app',
 	components: {
 		Modal,
 		ErrorMessageList,
-		ExperienceSlideshow
+		ExperienceSlideshow,
+		SkillsetShowcase
 	}
 });
