8dfb0a58cb6d3fdd96427453807935c484c8483d
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-25 23:27:21 -0500

    Update web.php
    Updated: web.php
    -> Added routes for the new SkillController methods.

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PositionController;
+use App\Http\Controllers\SkillController;
 use App\Http\Controllers\TextComponentController;
 
 /*
@@ -26,6 +27,7 @@ Route::post('/component/text', [TextComponentController::class, 'store']);
 
 Route::post('/positions', [PositionController::class, 'store']);
 
-
+Route::post('/skills', [SkillController::class, 'store']);
 
 Route::get('/api/positions', [PositionController::class, 'viewJson']);
+Route::get('/api/skills', [SkillController::class, 'viewJson']);
