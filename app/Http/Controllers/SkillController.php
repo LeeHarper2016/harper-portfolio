b8e879fa1c21ab35924a9bc01384ddd377df4a1a
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-25 23:02:25 -0500

    Update SkillController.php
    Updated: SkillController.php
    -> Implemented a store() method, which will add a new skill to the database based on user input.

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Skill;
 
 class SkillController extends Controller
 {
-    //
+    /****************************************************************************************************
+     * 
+     * Function: SkillController.store().
+     * Purpose: Stores a new skill into the database based on information provided by the user.
+     * Precondition: N/A.
+     * Postcondition: N/A.
+     * 
+     * @param Request $request The entire HTTP request.
+     * @returns json The JSON object that details the newly-stored skill.
+     * 
+    ****************************************************************************************************/
+    public function store(Request $request) {
+        $skill = Skill::create([
+            'name' => $request->skill_name,
+            'img_src' => 'images/uploaded/' . $request->image->hashName(),
+            'img_alt' => $request->img_alt
+        ]);
+
+        $request->image->store('/images/uploaded');
+
+        return json_encode($skill);
+    }
 }
