5110e3fac437cebfd00f77be47f94fc5a32738be
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-26 20:53:38 -0500

    Update welcome.blade.php
    Updated: welcome.blade.php
    -> Implemented the start of a contact form.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,6 +83,31 @@
                     </div>
                 @endif
                 <skillset-showcase></skillset-showcase>
+                <div class="p-8 w-full">
+                    <a name="contact"></a>
+                    <h2 class="font-bold text-2xl text-center">Contact Us</h2>
+                    <form class="flex flex-col items-center mt-5 space-y-5" method="POST" action="/contact">
+                        <div class="flex flex-row justify-between w-1/3">
+                            <label for="name" class="flex flex-col">
+                                Name:
+                                <input type="text" name="name" class="rounded-md border p-1.5">
+                            </label>
+                            <label for="email" class="flex flex-col">
+                                Email:
+                                <input type="email" name="email" class="rounded-md border p-1.5">
+                            </label>
+                            <label for="phone" class="flex flex-col">
+                                Phone:
+                                <input type="text" name="phone" class="rounded-md border p-1.5">
+                            </label>
+                        </div>
+                        <label for="body" class="flex flex-col">
+                            Body:
+                            <textarea name="body" rows="5" cols="40" class="rounded-md border p-1.5">
+                            </textarea>
+                        </label>
+                    </form>
+                </div>
             </main>
         </div>
     </body>
