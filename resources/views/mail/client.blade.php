4f240fedfe4e2b0ea2284e569f0aca85f1aee147
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-26 22:06:40 -0500

    Update client.blade.php
    Updated: client.blade.php
    -> Generated the structure & styling for the mail component.

--- resources/views/mail/client.blade.php
+++ resources/views/mail/client.blade.php
@@ -1,2 +1,36 @@
-<div>
-</div>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body>
+    	<div class="w-screen h-screen bg-gray-400 p-36">
+    		<div class="w-full h-full bg-white rounded-xl p-10 space-y-10 overflow-y-scroll">
+    			<h1 class="text-center font-bold text-2xl">Your Message Has Been Sent</h1>
+    			<p class="w-1/2 ml-auto mr-auto">
+    				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    			</p>
+    		</div>
+    	</div>
+    </body>
+</html>
