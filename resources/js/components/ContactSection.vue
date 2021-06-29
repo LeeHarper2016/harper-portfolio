1d5721061fe25cad4af6a99cadbf1e4f769d50b7
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-29 17:32:31 -0500

    Update ContactSection.vue
    Updated: ContactSection.vue
    -> Updated the syntax to accurately follow common Vue convention.

--- resources/js/components/ContactSection.vue
+++ resources/js/components/ContactSection.vue
@@ -1,24 +1,39 @@
 <template>
     <div class="p-8 w-full">
     <h2 class="font-bold text-3xl text-center">Contact Us</h2>
-    <form class="flex flex-col items-center mt-5 space-y-5" method="POST" action="/contact">
+    <form class="flex flex-col items-center mt-5 space-y-5" 
+    	  method="POST" 
+    	  action="/contact">
         <div class="flex flex-row justify-between w-1/3">
-            <label for="name" class="flex flex-col">
+            <label for="name" 
+            	   class="flex flex-col">
                 Name:
-                <input type="text" name="name" class="rounded-md border p-1.5">
+                <input type="text" 
+                	   name="name" 
+                	   class="rounded-md border p-1.5">
             </label>
-            <label for="email" class="flex flex-col">
+            <label for="email" 
+            	   class="flex flex-col">
                 Email:
-                <input type="email" name="email" class="rounded-md border p-1.5">
+                <input type="email" 
+                	   name="email" 
+                	   class="rounded-md border p-1.5">
             </label>
-            <label for="phone" class="flex flex-col">
+            <label for="phone" 
+            	   class="flex flex-col">
                 Phone:
-                <input type="text" name="phone" class="rounded-md border p-1.5">
+                <input type="text" 
+                	   name="phone" 
+                	   class="rounded-md border p-1.5">
             </label>
         </div>
-        <label for="body" class="flex flex-col">
+        <label for="body" 
+        	   class="flex flex-col">
             Body:
-            <textarea name="body" rows="5" cols="40" class="rounded-md border p-1.5">
+            <textarea name="body" 
+            		  rows="5" 
+            		  cols="40" 
+            		  class="rounded-md border p-1.5">
             </textarea>
         </label>
     </form>
