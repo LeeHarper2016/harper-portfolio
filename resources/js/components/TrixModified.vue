fe600253f9a6fde889346cbf751509084353d29f
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-30 02:14:56 -0500

    Create TrixModified.vue
    Created: TrixModified.vue
    -> Created a vue component to host the trix-editor.
    -> This will allow the editor to be interacted with in a more versatile way, such as populating the field with content.
    -> Attachments are disabled for the time being.

---
+++ resources/js/components/TrixModified.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <input :id="this.inputId"
+               type="hidden"
+               :name="this.inputName"
+               :value="this.content">
+        <trix-editor :id="this.inputId"
+                 :input="this.inputId"
+                 :name="this.inputName"
+                 :placeholder="this.placeholder"
+                 v-model="this.content"></trix-editor>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'TrixModified',
+      props: [
+          'content',
+          'placeholder',
+          'input-name',
+          'input-id'
+      ],
+      data() {
+          return {
+              editorContent: null,
+          }
+      },
+      mounted() {
+          // Gets the file button from the trix editor.
+          let fileButton = document.getElementsByClassName('trix-button--icon-attach')[0];
+
+          // Hides the file button due to it not being used.
+          fileButton.parentElement.style.visibility = 'hidden';
+
+          if (this.content) {
+              this.editorContent = this.content;
+          }
+      }
+    }
+</script>
+
+<style scoped>
+</style>
