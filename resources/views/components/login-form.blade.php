7eed933d40f82e835f6823e6c2df1b738713e618
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-30 02:47:17 -0500

    Update login-form.blade.php
    Updated: login-form.blade.php
    -> Added padding and placeholder text to all form elements.

--- resources/views/components/login-form.blade.php
+++ resources/views/components/login-form.blade.php
@@ -3,11 +3,11 @@
         @csrf
         <label for="name">
             Username:
-            <input type="text" name="name" id="name" class="border rounded-md block">
+            <input type="text" name="name" id="name" class="border rounded-md block p-1.5" placeholder="User">
         </label>
         <label for="password">
             Password:
-            <input type="password" name="password" id="password" class="border rounded-md block">
+            <input type="password" name="password" id="password" class="border rounded-md block p-1.5" placeholder="Password">
         </label>
         <button type="submit" class="border border-purple-800 rounded-lg p-3 bg-purple-300">
             Log In
