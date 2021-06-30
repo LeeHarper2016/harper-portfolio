3fa7bbca40c2d46d7683aace9368f7416d4d6fbb
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-29 19:55:23 -0500

    Create SkillRequest.php
    Created: SkillRequest.php
    -> This class will be responsible for validating the user input regarding the SkillController.

---
+++ app/Http/Requests/SkillRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SkillRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return !is_null(auth()->user());
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required'],
+            'img_src' => 'images/uploaded/' . $request->image->hashName(),
+            'img_alt' => ['required']
+        ];
+    }
+}
