f8e65835d5bc2c0be77e7c7f8b3057d5f98f2ed6
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-29 20:03:31 -0500

    Create ContactRequest.php
    Created: ContactRequest.php
    -> This class will be responsible for validating the data that the user supplies through the Contact form.
    -> The phone number is validated through a regular expression, which will allow multiple types of valid phone number inputs to be handled.

---
+++ app/Http/Requests/ContactRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required'],
+            'email' => ['required', 'email'],
+            'phone' => ['required', 'regex:/\(?\d{3}\)?\s?[0-9]\d{2}-?\d{4}/'],
+            'body' => ['required']
+       ];
+    }
+}
