b1e23bf00a493ca825b9cce4483d118a2fc48ce7
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-06-30 20:14:46 -0500

    Update welcome.blade.php
    Updated: welcome.blade.php
    -> Changed the formatting of the header, as it was causing an x-overflow.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
         <div id="app">
             <error-message-list :errors='@json($errors->all())'>
             </error-message-list>
-            <header class="w-screen h-screen bg-header-bg bg-cover bg-fixed">
+            <header class="w-full h-screen bg-header-bg bg-cover bg-fixed">
                 <nav class="flex justify-around p-5 font-bold">
                     <a href="#about" class="hover:text-gray-800 text-xl duration-300">About</a>
                     <a href="#experience" class="hover:text-gray-800 text-xl duration-300">Experience</a>
