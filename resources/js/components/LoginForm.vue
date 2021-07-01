088cbdaa5cd21991613318b66e30407fa5a28ec5
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-01 02:47:15 -0500

    Create LoginForm.vue
    Created: LoginForm.vue
    -> Refactored the login form into its own Vue component.

---
+++ resources/js/components/LoginForm.vue
@@ -0,0 +1,43 @@
+<template>
+    <form action="/login" 
+    	  method="POST" 
+    	  class="flex flex-col items-center space-y-6 font-normal">
+	  <input type="hidden"
+	  		 name="_token"
+	  		 :value="csrfToken">
+        <label for="name">
+            Username:
+            <input type="text" 
+            	   name="name" 
+            	   id="name" 
+            	   class="border rounded-md block p-1.5" 
+            	   placeholder="User">
+        </label>
+        <label for="password">
+            Password:
+            <input type="password" 
+            	   name="password" 
+            	   id="password" 
+            	   class="border rounded-md block p-1.5" 
+            	   placeholder="Password">
+        </label>
+        <button type="submit" 
+        		class="border border-purple-800 rounded-lg p-3 bg-purple-300">
+            Log In
+        </button>
+    </form>
+</template>
+
+<style scoped>
+</style>
+
+<script type="text/javascript">
+	export default {
+		name: 'LoginForm',
+		computed: {
+			csrfToken() {
+            	return document.querySelector('meta[name="csrf-token"]').content;
+			}
+		}
+	};
+</script>
