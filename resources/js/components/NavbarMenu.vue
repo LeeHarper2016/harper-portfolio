0731cb3e6c2e716459c9657f1984ef063f2c6a45
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-01 03:45:57 -0500

    Update NavbarMenu.vue
    Updated: NavbarMenu.vue
    -> Replaced the old implementation of menu items with the new NavbarMenuItems vue component.

--- resources/js/components/NavbarMenu.vue
+++ resources/js/components/NavbarMenu.vue
@@ -1,24 +1,14 @@
 <template>
     <nav class="flex flex-col overflow-hidden h-20 bg-white justify-between p-5 font-bold border-purple-800 border-b-2 transition-all duration-400 md:flex-row md:bg-transparent md:border-none"
     	 ref="navbarMenu">
-        <a @click.prevent="toggleCollapse" class="text-xl duration-300 rounded-md p-2 cursor-pointer hover:bg-gray-100 hover:text-gray-700 md:hidden">Menu</a>
-        <a href="#about" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">About</a>
-        <a href="#experience" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Experience</a>
-        <a href="#education" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Education</a>
-        <a href="#skills" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Skills</a>
-        <a href="#contact" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Contact</a>
-        <modal v-if="!this.user" class="inline" button_classes="font-bold text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">
-            <template #button_text>
-                Login
-            </template>
-            <template #header>
-                Log In
-            </template>
-            <template #content>
-				<LoginForm></LoginForm>
-            </template>
-        </modal>
-        <a v-else href="/logout" class="font-bold text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Log Out</a>
+    	<NavbarMenuItem type="menuButton">Menu</NavbarMenuItem>
+        <NavbarMenuItem anchor="#about">About</NavbarMenuItem>
+        <NavbarMenuItem anchor="#experience">Experience</NavbarMenuItem>
+        <NavbarMenuItem anchor="#education">Education</NavbarMenuItem>
+        <NavbarMenuItem anchor="#skills">Skills</NavbarMenuItem>
+        <NavbarMenuItem anchor="#contact">Contact</NavbarMenuItem>
+        <NavbarMenuItem v-if="!this.user" type="loginButton">Login</NavbarMenuItem>
+        <NavbarMenuItem v-else type="logoutButton">Log Out</NavbarMenuItem>
     </nav>
 </template>
 
@@ -26,8 +16,7 @@
 </style>
 
 <script type="text/javascript">
-	import LoginForm from './LoginForm';
-	import Modal from './Modal';
+	import NavbarMenuItem from './NavbarMenuItem';
 
 	export default {
 		name: 'NavbarMenu',
@@ -38,8 +27,7 @@
 			}
 		},
 		components: {
-			LoginForm,
-			Modal
+			NavbarMenuItem
 		},
 		methods: {
 			toggleCollapse() {
