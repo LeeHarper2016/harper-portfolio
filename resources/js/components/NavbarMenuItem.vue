1fde49f2d3242851862969eb190516b3b27123f1
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-01 03:45:19 -0500

    Create NavbarMenuItem.vue
    Created: NavbarMenuItem.vue
    -> Created a Vue component for elements residing inside of the navbar.
    -> This is mainly to clean up the formatting of the original component, as well as future-proofing the component if it ever needs special functionality.

---
+++ resources/js/components/NavbarMenuItem.vue
@@ -0,0 +1,46 @@
+<template>
+    <a v-if="type === 'menuButton'"
+       class="text-xl duration-300 rounded-md p-2 cursor-pointer hover:bg-gray-100 hover:text-gray-700 md:hidden">
+           <slot></slot>
+       </a>
+    <modal v-else-if="type === 'loginButton'"
+    	   class="inline" 
+    	   button_classes="font-bold text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">
+        <template #button_text>
+            <slot></slot>
+        </template>
+        <template #header>
+            Log In
+        </template>
+        <template #content>
+			<LoginForm></LoginForm>
+        </template>
+    </modal>
+    <a v-else-if="type === 'logoutButton'"
+       href="/logout"
+       class="font-bold text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">
+           <slot></slot>
+       </a>
+    <a v-else
+       :href="anchor"
+       class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">
+    	<slot></slot>
+    </a>
+</template>
+
+<style scoped>
+</style>
+
+<script type="text/javascript">
+    import LoginForm from './LoginForm';
+	import Modal from './Modal';
+
+	export default {
+		name: 'NavbarMenuItem',
+		props: ['anchor', 'type'],
+		components: {
+            LoginForm,
+			Modal
+		}
+	};
+</script>
