04accaf872e8bb7f7c1867b5b84203874d433450
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-01 01:31:04 -0500

    Update NavbarMenu.vue
    Updated: NavbarMenu.vue
    -> Implemented a menu button, which will collapse/decollapse the navbar menu on smaller devices.
    -> Removed the transition tag and styling, as all transitions done are supported by TailwindCSS.
    -> Implemented methods and watchers that will operate the collapsible menu.

--- resources/js/components/NavbarMenu.vue
+++ resources/js/components/NavbarMenu.vue
@@ -1,22 +1,16 @@
 <template>
-	<transition name="fade">
-        <nav class="flex flex-col bg-white justify-around p-5 font-bold border-purple-800 border-b-2 md:flex-row md:bg-transparent md:border-none">
-            <a href="#about" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">About</a>
-            <a href="#experience" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Experience</a>
-            <a href="#education" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Education</a>
-            <a href="#skills" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Skills</a>
-            <a href="#contact" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700 hover:scale-125">Contact</a>
-        </nav>
-	</transition>
+    <nav class="flex flex-col overflow-hidden h-20 bg-white justify-between p-5 font-bold border-purple-800 border-b-2 transition-all duration-400 md:flex-row md:bg-transparent md:border-none"
+    	 ref="navbarMenu">
+        <a @click.prevent="toggleCollapse" class="text-xl duration-300 rounded-md p-2 cursor-pointer hover:bg-gray-100 hover:text-gray-700 md:hidden">Menu</a>
+        <a href="#about" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">About</a>
+        <a href="#experience" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Experience</a>
+        <a href="#education" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Education</a>
+        <a href="#skills" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Skills</a>
+        <a href="#contact" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700 hover:scale-125">Contact</a>
+    </nav>
 </template>
 
 <style scoped>
-.fade-enter-active, .fade-leave-active {
-	transition: opacity 0.65s ease;
-}
-.fade-enter, .fade-leave-to {
-	opacity: 0;
-}
 </style>
 
 <script type="text/javascript">
@@ -28,6 +22,18 @@
 			}
 		},
 		methods: {
+			toggleCollapse() {
+				this.collapsed = !this.collapsed;
+			}
+		},
+		watch: {
+	        collapsed() {
+	            if (this.collapsed) {
+	                this.$refs['navbarMenu'].style.height = "4.75rem";
+	            } else {
+	                this.$refs['navbarMenu'].style.height = "18rem";
+	            }
+	        }
 		}
 	};
 </script>
