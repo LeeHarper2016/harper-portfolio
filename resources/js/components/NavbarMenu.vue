3820673a59558394cdfac06029156995b0e4de9f
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-01 01:16:17 -0500

    Update NavbarMenu.vue
    Updated: NavbarMenu.vue
    -> Implemented hover effects for the background of items in the navbar.
    -> Added padding and rounded the corners of the elements inside of the navbar.
    -> Removed header and subheader sections, as they were not meant to be included in the navbar menu refactor.

--- resources/js/components/NavbarMenu.vue
+++ resources/js/components/NavbarMenu.vue
@@ -1,18 +1,12 @@
 <template>
 	<transition name="fade">
-        <nav class="flex flex-col bg-white justify-around p-5 font-bold border-purple-800 border-b-2 md:flex-row md:bg-transparent">
-            <a href="#about" class="text-xl duration-300 hover:text-gray-700">About</a>
-            <a href="#experience" class="text-xl duration-300 hover:text-gray-700">Experience</a>
-            <a href="#education" class="text-xl duration-300 hover:text-gray-700">Education</a>
-            <a href="#skills" class="text-xl duration-300 hover:text-gray-700">Skills</a>
-            <a href="#contact" class="text-xl duration-300 hover:text-gray-700 hover:scale-125">Contact</a>
+        <nav class="flex flex-col bg-white justify-around p-5 font-bold border-purple-800 border-b-2 md:flex-row md:bg-transparent md:border-none">
+            <a href="#about" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">About</a>
+            <a href="#experience" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Experience</a>
+            <a href="#education" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Education</a>
+            <a href="#skills" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700">Skills</a>
+            <a href="#contact" class="text-xl duration-300 rounded-md p-2 hover:bg-gray-100 hover:text-gray-700 hover:scale-125">Contact</a>
         </nav>
-        <div class="flex flex-col w-full h-full justify-center mr-auto ml-auto items-center">
-            <div class="flex flex-col justify-around w-1/3 h-1/3 bg-white bg-opacity-80 rounded-xl p-5 text-center">
-                <h1 class="block font-bold text-4xl underline">Page Header</h1>
-                <h2 class="block font-bold text-3xl underline">Page Subheader</h2>
-            </div>
-        </div>
 	</transition>
 </template>
 
