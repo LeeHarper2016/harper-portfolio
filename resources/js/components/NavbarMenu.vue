65c0c1a5c48210e9b42589af47cd3c03cb9bcc04
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-02 13:14:15 -0500

    Update NavbarMenu.vue
    Updated: NavbarMenu.vue
    -> Updated the menu to, if on mobile devices, close out of the menu once an option is clicked.

--- resources/js/components/NavbarMenu.vue
+++ resources/js/components/NavbarMenu.vue
@@ -3,17 +3,25 @@
     	 ref="navbarMenu">
     	<NavbarMenuItem @menuToggled="toggleCollapse" 
     					type="menuButton">Menu</NavbarMenuItem>
-        <NavbarMenuItem anchor="#about">About</NavbarMenuItem>
-        <NavbarMenuItem anchor="#experience">Experience</NavbarMenuItem>
-        <NavbarMenuItem anchor="#education">Education</NavbarMenuItem>
-        <NavbarMenuItem anchor="#skills">Skills</NavbarMenuItem>
-        <NavbarMenuItem anchor="#contact">Contact</NavbarMenuItem>
-        <NavbarMenuItem v-if="!this.user" type="loginButton">Login</NavbarMenuItem>
-        <NavbarMenuItem v-else type="logoutButton">Log Out</NavbarMenuItem>
+        <NavbarMenuItem @itemClicked="setCollapse(true)" anchor="#about">About</NavbarMenuItem>
+        <NavbarMenuItem @itemClicked="setCollapse(true)" anchor="#experience">Experience</NavbarMenuItem>
+        <NavbarMenuItem @itemClicked="setCollapse(true)" anchor="#education">Education</NavbarMenuItem>
+        <NavbarMenuItem @itemClicked="setCollapse(true)" anchor="#skills">Skills</NavbarMenuItem>
+        <NavbarMenuItem @itemClicked="setCollapse(true)" anchor="#contact">Contact</NavbarMenuItem>
+        <NavbarMenuItem @itemClicked="setCollapse(true)" v-if="!this.user" type="loginButton">Login</NavbarMenuItem>
+        <NavbarMenuItem @itemClicked="setCollapse(true)" v-else type="logoutButton">Log Out</NavbarMenuItem>
     </nav>
 </template>
 
 <style scoped>
+.fixedNavbar {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	background: white;
+	border-bottom: 2px solid rgba(96, 165, 250);
+}
 </style>
 
 <script type="text/javascript">
@@ -24,15 +32,26 @@
 		props: ['user'],
 		data() {
 			return {
-				collapsed: true
+				collapsed: true,
+				fixed: false
 			}
 		},
 		components: {
 			NavbarMenuItem
 		},
 		methods: {
+			setCollapse(isCollapsed) {
+				this.collapsed = isCollapsed;
+			},
 			toggleCollapse() {
 				this.collapsed = !this.collapsed;
+			},
+			updateFixedNavbar() {
+	        	if (window.scrollY > this.$refs['navbarMenu'].clientHeight) {
+	        		this.$refs['navbarMenu'].classList.add('fixedNavbar');
+	        	} else {
+	        		this.$refs['navbarMenu'].classList.remove('fixedNavbar');
+	        	}
 			}
 		},
 		watch: {
@@ -43,6 +62,11 @@
 	                this.$refs['navbarMenu'].style.height = "18rem";
 	            }
 	        }
+		},
+		mounted() {
+			this.updateFixedNavbar();
+
+			window.addEventListener('scroll', this.updateFixedNavbar);
 		}
 	};
 </script>
