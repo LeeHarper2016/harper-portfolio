7cc3009c8c8da20d7a583fcd7be971dbda2aa1bb
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-02 04:56:24 -0500

    Update welcome.blade.php
    Updated: welcome.blade.php
    -> Implemented favicon functionality.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,10 @@
 
         <title>{{ env('APP_NAME') }}</title>
 
-        <!-- Fonts -->
+        <!-- Favicon -->
+        <link rel=“shortcut icon” type=“image/x-icon” href=“favicon.ico”>
+
+        <!-- Styles -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
