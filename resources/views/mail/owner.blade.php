6379ebeb1205bc033ca2c5973a3177d3850e0f46
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-02 04:47:19 -0500

    Update owner.blade.php
    Updated: owner.blade.php
    ->Fixed an issue where the wrong variable name was being used.

--- resources/views/mail/owner.blade.php
+++ resources/views/mail/owner.blade.php
@@ -2,7 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-
         <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
@@ -11,13 +10,13 @@
     		<div class="w-full h-full bg-white text-center rounded-xl p-10 space-y-10 overflow-y-scroll">
     			<h1 class="text-center font-bold text-2xl">You Have a Message!</h1>
                 <div class="w-1/2 space-y-5 ml-auto mr-auto">
-                    <span class="block">{{ $message->name }} has sent you a message at {{ date('Y-m-d', time()) }}</span>
+                    <span class="block">{{ $messageData->name }} has sent you a message at {{ date('Y-m-d', time()) }}</span>
                     <span class="block">Message Body:</span>
                     <div>
-                        {{ $message->contact_body }}
+                        {{ $messageData->contact_body }}
                     </div>
                     <div>
-                        You can contact {{ $message->name }} at {{ $message->email }} and {{ $message->phone }}.
+                        You can contact {{ $messageData->name }} at {{ $messageData->email }} and {{ $message->phone }}.
                     </div>
                 </div>
     		</div>
