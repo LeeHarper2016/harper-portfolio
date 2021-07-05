6f4104e43aada60761baca169122a0940ebee27d
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-05 13:13:59 -0500

    Update owner.blade.php
    Updated: owner.blade.php
    -> Reduced the formatting of the email in order to be more compatible with a variety of email providers.
    -> Removed the javascript tag, as it isn't required for emails.

--- resources/views/mail/owner.blade.php
+++ resources/views/mail/owner.blade.php
@@ -3,23 +3,19 @@
     <head>
         <meta charset="utf-8">
         <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
-    	<div class="w-screen h-screen bg-gray-400 p-36">
-    		<div class="w-full h-full bg-white text-center rounded-xl p-10 space-y-10 overflow-y-scroll">
-    			<h1 class="text-center font-bold text-2xl">You Have a Message!</h1>
-                <div class="w-1/2 space-y-5 ml-auto mr-auto">
-                    <span class="block">{{ $messageData->name }} has sent you a message at {{ date('Y-m-d', time()) }}</span>
-                    <span class="block">Message Body:</span>
-                    <div>
-                        {{ $messageData->contact_body }}
-                    </div>
-                    <div>
-                        You can contact {{ $messageData->name }} at {{ $messageData->email }} and {{ $message->phone }}.
-                    </div>
+    	<div>
+            <h1 class="text-lg font-bold">You have recieved a message</h1>
+                <span class="block">{{ $messageData->name }} has sent you a message at {{ date('Y-m-d', time()) }}</span>
+                <span class="block">Message Body:</span>
+                <div class="block">
+                    {{ $messageData->contact_body }}
                 </div>
-    		</div>
-    	</div>
+                <div class="block">
+                    You can contact {{ $messageData->name }} at {{ $messageData->email }} and {{ $message->phone }}.
+                </div>
+            </div>
+		</div>
     </body>
 </html>
