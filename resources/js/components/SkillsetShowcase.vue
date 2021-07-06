42e673f584c8653c355809ebbdff30c21fcf862e
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-06 04:35:16 -0500

    Update SkillsetShowcase.vue
    Updated: SkillsetShowcase.vue
    -> Edited the styling of the modal button, which allows it to sit beside the header rather than under it.

--- resources/js/components/SkillsetShowcase.vue
+++ resources/js/components/SkillsetShowcase.vue
@@ -2,34 +2,35 @@
     <div class="p-8 w-full">
     	<div class="text-center">
 	        <h2 class="inline-block font-bold text-3xl mb-3 mr-5">My Skillset</h2>
-	        <hr class="border-blue-800 w-full ml-auto mr-auto md:w-1/4">
-    	</div>
-        <div v-if="user"
-        	 class="flex justify-center p-5">
-		    <Modal class="inline" 
-		    	   button_classes="bg-blue-200 rounded-lg p-3 border border-blue-800">
-		        <template #button_text>
-		            Edit Skills
-		        </template>
-		        <template #header>
-		        	Add A Skill
-		        </template>
-		        <template #content>
-		        	<SkillsetShowcaseForm @skillAdded="addSkill($event)"></SkillsetShowcaseForm>
+            <div v-if="user"
+            	 class="inline">
+			    <Modal class="inline"
+			    	   button_classes="bg-blue-200 rounded-lg p-3 border border-blue-800">
+			        <template #button_text>
+			            Edit Skills
+			        </template>
+			        <template #header>
+			        	Add A Skill
+			        </template>
+			        <template #content>
+			        	<SkillsetShowcaseForm @skillAdded="addSkill($event)"></SkillsetShowcaseForm>
 
-		        	<h2 class="font-bold text-center text-3xl mt-10">Skills</h2>
-		        	<div class="flex flex-wrap justify-center w-2/3 mr-auto ml-auto">
-			        	<div v-for="skill of skills"
-			        		 :key="skill._id"
-			        		 class="w-48 text-center m-5 rounded-md bg-blue-300 border border-blue-800 p-3">
-			        		 <span class="w-full text-xl font-bold">{{ skill.name }}</span>
-			        		 <span @click="deleteSkill(skill._id)"
-			        		 	   class="block cursor-pointer p-1.5 bg-red-300 rounded-md border border-2 border-black">Delete</span>
+			        	<h2 class="font-bold text-center text-3xl mt-10">Skills</h2>
+			        	<div class="flex flex-wrap justify-center w-2/3 mr-auto ml-auto">
+				        	<div v-for="skill of skills"
+				        		 :key="skill._id"
+				        		 class="w-48 text-center m-5 rounded-md bg-blue-300 border border-blue-800 p-3">
+				        		 <span class="w-full text-xl font-bold">{{ skill.name }}</span>
+				        		 <span @click="deleteSkill(skill._id)"
+				        		 	   class="block cursor-pointer p-1.5 bg-red-300 rounded-md border border-2 border-black">Delete</span>
+				        	</div>
 			        	</div>
-		        	</div>
-		        </template>
-		    </Modal>
-        </div>
+			        </template>
+			    </Modal>
+	        </div>
+	        <hr class="border-blue-800 w-full ml-auto mr-auto mt-2 mb-2 md:w-1/4">
+    	</div>
+
         <div class="flex flex-wrap justify-center w-2/3 mr-auto ml-auto">
         	<div v-for="skill of skills"
         		 :key="skill._id"
