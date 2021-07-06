5a02302710c5fcab0608bd6dd6f57f2eac60c47d
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-06 04:07:11 -0500

    Update SkillsetShowcase.vue
    Updated: SkillsetShowcase.vue
    -> Implemented a display for a list of skills inside of the modal. Models can be deleted from this list.
    -> Implemented a deleteSkill() method, which would first make a DELETE request to the '/skills/{id}' route, then removes the deleted skill from the list of skills.
    -> Fixed an error where the "id" field was referenced instead of the "_id".

--- resources/js/components/SkillsetShowcase.vue
+++ resources/js/components/SkillsetShowcase.vue
@@ -9,19 +9,30 @@
 		    <Modal class="inline" 
 		    	   button_classes="bg-blue-200 rounded-lg p-3 border border-blue-800">
 		        <template #button_text>
-		            Add Skill
+		            Edit Skills
 		        </template>
 		        <template #header>
 		        	Add A Skill
 		        </template>
 		        <template #content>
 		        	<SkillsetShowcaseForm @skillAdded="addSkill($event)"></SkillsetShowcaseForm>
+
+		        	<h2 class="font-bold text-center text-3xl mt-10">Skills</h2>
+		        	<div class="flex flex-wrap justify-center w-2/3 mr-auto ml-auto">
+			        	<div v-for="skill of skills"
+			        		 :key="skill._id"
+			        		 class="w-48 text-center m-5 rounded-md bg-blue-300 border border-blue-800 p-3">
+			        		 <span class="w-full text-xl font-bold">{{ skill.name }}</span>
+			        		 <span @click="deleteSkill(skill._id)"
+			        		 	   class="block cursor-pointer p-1.5 bg-red-300 rounded-md border border-2 border-black">Delete</span>
+			        	</div>
+		        	</div>
 		        </template>
 		    </Modal>
         </div>
         <div class="flex flex-wrap justify-center w-2/3 mr-auto ml-auto">
         	<div v-for="skill of skills"
-        		 :key="skill.id"
+        		 :key="skill._id"
         		 class="w-48 text-center m-5 rounded-md bg-blue-300 border border-blue-800 p-3">
         		<span v-text="skill.name"
         			  class="w-full text-xl font-bold"></span>
@@ -58,6 +69,13 @@
 		methods: {
 			addSkill(skill) {
 				this.skills.push(skill);
+			},
+			async deleteSkill(skill_id) {
+				let res = await axios.delete('/skills/' + skill_id);
+
+				let deletedSkill = res.data;
+
+				this.skills = this.skills.filter((skill) => skill._id !== deletedSkill._id);
 			}
 		},
 		async mounted() {
