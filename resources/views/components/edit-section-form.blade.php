afcc5bb93af82c34bf026d8c21532db5cab61894
LeeHarper2016 <42632613+LeeHarper2016@users.noreply.github.com>
2021-07-06 00:27:58 -0500

    Update edit-section-form.blade.php
    Updated: edit-section-form.blade.php
    -> Implemented the new $section field, which will be used to populate the fields inside of the form.
    -> Made the form method a PATCH request, as this will be used for updating models.

--- resources/views/components/edit-section-form.blade.php
+++ resources/views/components/edit-section-form.blade.php
@@ -1,12 +1,14 @@
-<form action="/component/text" method="POST" class="flex flex-col items-center space-y-6 font-normal" enctype="multipart/form-data">
+<form action="/component/text/{{ $section->id }}" method="POST" class="flex flex-col items-center space-y-6 font-normal" enctype="multipart/form-data">
     @csrf
-    <input type="hidden" name="name" id="name" value="{{ $anchor ?? null }}" class="border rounded-md block">
+    @method('PATCH')
+    <input type="hidden" name="name" id="name" value="{{ $section->name ?? null }}" class="border rounded-md block">
     <label for="title">
         Section Title:
-        <input type="text" name="title" id="title" class="border rounded-md block p-1.5" placeholder="Section Name">
+        <input type="text" name="title" id="title" class="border rounded-md block p-1.5" placeholder="Section Name" value="{{ $section->title ?? null }}">
     </label>
     <label for="body">
-        <trix-modified input-id="body" input-name="body" placeholder="A summary of this section"></trix-modified>
+        Section Body:
+        <trix-modified input-id="body" input-name="body" placeholder="A summary of this section" content="{{ $section->body ?? null }}"></trix-modified>
     </label>
     <label for="image">
         Section Image:
@@ -14,9 +16,7 @@
     </label>
     <label for="img_alt">
         Image Description:
-        <textarea cols="50" rows="5" id="img_alt" name="img_alt" class="border rounded-md block p-1.5" placeholder="A small description of the image.">
-            
-        </textarea>
+        <textarea cols="50" rows="5" id="img_alt" name="img_alt" class="border rounded-md block p-1.5" placeholder="A small description of the image.">{{ $section->img_alt ?? null }}</textarea>
     </label>
     <button type="submit" class="border border-blue-800 rounded-lg p-3 bg-blue-300">
         Submit
